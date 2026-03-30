737954cbc5616ecd814ffad593f0f1e8da810607
Yoon Ji Hoon <wlgns7922@gmail.com>
2026-03-30 21:00:35 +0900

    프래그먼트 셰이더 연습

--- SimpleGame/Shaders/FS.fs
+++ SimpleGame/Shaders/FS.fs
@@ -9,13 +9,16 @@ void main()
 	// 주어진 정점 정보를 통해, 폴리곤 내부의 픽셀들에 대해 연산을 수행
 	// 500x500 크기의 창을 꽉 채울 경우 25만개의 픽셀에 대한 연산 수행
 
-	if (v_TPos.x > 0.66) {
+	if (v_TPos.x > 0.5) {
 		FragColor = vec4(1, 1, 1, 1);
-	}
-	else if (v_TPos.x > 0.33) {
-		FragColor = vec4(0.5, 0.5, 0.5, 1);
 	}
 	else {
 		FragColor = vec4(0, 0, 0, 1);
+	}
+
+	float dist = sqrt(pow(v_TPos.x * 2 - 1, 2) + pow(v_TPos.y * 2 - 1, 2));
+
+	if (dist > 0.5f){
+		FragColor = vec4(0, 0, 1, 1);
 	}
 }
